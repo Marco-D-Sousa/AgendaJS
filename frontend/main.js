8ead4a1d8bb411ace4d7436e441b964ee1b1d393
Marco de Sousa <marcopaiva2004@gmail.com>
2022-09-20 18:30:28 -0300

    Iniciando o repositório do projeto

---
+++ frontend/main.js
@@ -0,0 +1,7 @@
+import Login from "./forms/Login";
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
---
+++ routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const route = express.Router();
+
+const homeControl = require('./src/controllers/homeControl');
+const loginControl = require('./src/controllers/loginControl');
+const contatoControl = require('./src/controllers/contatoControl');
+
+const { loginRequired } = require('./src/middlewares/middleware');
+
+//Rotas da Home
+route.get('/', homeControl.index);
+
+//Rotasd de Login
+route.get('/login/index', loginControl.index);
+route.post('/login/register', loginControl.register);
+route.post('/login/login', loginControl.login);
+route.get('/login/logout', loginControl.logout);
+
+//Rotas de Contato
+route.get('/contato/index', loginRequired, contatoControl.index);
+route.post('/contato/register', loginRequired, contatoControl.register);
+route.get('/contato/index/:id', loginRequired, contatoControl.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoControl.edit);
+route.get('/contato/delete/:id', loginRequired, contatoControl.delete);
+
+module.exports = route;
